453d887885ddccb8899190257686e93e0460b40c
Una L <una.lestrange@atu.ie>
2023-09-28 12:52:19 +0100

    finished ws2

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const app = express()
-const port = 3000
-
 
 require('dotenv').config();
 // note this required a .env file which is not in github
+
+const app = express()
+const port = process.env.PORT || 3001
 
 // own  modules
 const db = require('./database');
@@ -17,11 +17,9 @@ const home = require('./routes/home')
 
 app.use(express.json());
 
-app.use('/api/v1/contacts', contacts)
+app.use('/api/v1/contacts/', contacts)
 
 app.use('/', home)
-
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
