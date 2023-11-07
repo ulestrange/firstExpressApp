965b1f4a0cfe9988fa9b4af082db3cb27b2a42b9
Una L <una.lestrange@atu.ie>
2023-11-07 09:40:02 +0000

    added cors

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 
+
+const cors = require('cors')
 require('dotenv').config();
 // note this required a .env file which is not in github
+
+
 
 const app = express()
 const port = process.env.PORT || 3001
@@ -12,10 +16,13 @@ const contacts = require('./routes/contacts')
 const events = require('./routes/events')
 const home = require('./routes/home')
 
+var corsOptions = {
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
 
 
-
-
+app.use(cors(corsOptions))
 app.use(express.json());
 
 app.use('/api/v1/contacts/', contacts)
